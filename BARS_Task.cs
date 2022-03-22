5f67871c2fede910c69c6fc0a70485fed6d2d07c
AsolSailin <101903156+AsolSailin@users.noreply.github.com>
2022-03-23 00:34:43 +0300

    Add files via upload

---
+++ BARS_Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BARS_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subs = new Key();
+            subs.OnKeyPressed += (_, c) => Console.WriteLine(c);
+
+            subs.Run();
+        }
+    }
+    
+    public class Key
+    {
+        public event EventHandler <char> OnKeyPressed;
+
+        public void Run()
+        {
+            char key = Console.ReadKey().KeyChar;
+            while (key != 'c')
+            {
+                OnKeyPressed?.Invoke(this, key);
+            }
+        }
+    }
+}
