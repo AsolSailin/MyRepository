d414f8583ef6d192d70a73b86d30e4e16dbc5c74
Руслана Гараева <garaeva-r_23@mail.ru>
2022-03-31 00:45:51 +0300

    BARS

---
+++ BARS_Task2.1/LocalFileLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace BARS_Task2
+{
+    // потом когда класс этот интерфейс наследует он ОБЯЗАН реализовать все методы которые были в интерфейсе
+    // То теперь вон красным черкает, читаем. он не реализует этот метод а значит не выполняет
+    // условия "контракта" (интерфейса) по правилам он обязан реализовать все методы
+    // всё, все методы реализованы и он больше не ругается
+    // понятно
+    public class LocalFileLogger<T> : ILogger
+    {
+        private string fileLocation;
+
+        public LocalFileLogger(string location)
+        {
+            this.fileLocation = location;
+        }
+
+        public void LogInfo(string message)
+        {
+            File.AppendAllText(fileLocation, $"[Info]: [{typeof(T).Name}]:{message}" + '\n');
+        }
+
+        public void LogWarning(string message)
+        {
+            File.AppendAllText(fileLocation, $"[Warning]: [{typeof(T).Name}]: {message}" + '\n');
+        }
+
+        public void LogError(string message, Exception ex)
+        {
+            File.AppendAllText(fileLocation, $"[Error]: [{typeof(T).Name}]: {message}.{ex.Message}" + '\n');
+        }
+
+    }
+}
---
+++ BARS_Task2.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BARS_Task2
+{
+    class Program
+    {
+        private static string fileLocation = "./test.txt"; 
+        // . - это текущая папка (из которой у тебя программа запускается), test.txt название файла в который он писать будет
+        // программа у тебя запускается из папки bin в разделе который тут указан
+        // вроде всо, можно удалять комменты? или сама? ну ты все поняла? то что сверху написано и про интерфейсы
+        // про интерфейсы нет
+        // все равно, могу и сама
+
+        public static void Main(string[] args)
+        {
+            var intLogger1 = new LocalFileLogger<int>(fileLocation);
+            intLogger1.LogInfo("LogInfo int test passed");
+            intLogger1.LogWarning("LogWarning int test passed");
+            intLogger1.LogError("LogWarning int test passed", new Exception("Exception int"));
+
+            var stringLogger = new LocalFileLogger<string>(fileLocation);
+            stringLogger.LogInfo("LogInfo string test passed");
+            stringLogger.LogWarning("LogWarning string test passed");
+            stringLogger.LogError("LogWarning string test passed", new Exception("Exception string "));
+
+            var variablesLogger = new LocalFileLogger<Variables>(fileLocation);
+            variablesLogger.LogInfo("LogInfo variables test passed");
+            variablesLogger.LogWarning("LogWarning variables test passed");
+            variablesLogger.LogError("LogWarning variables test passed", new Exception("Exception variables"));
+        }
+    }
+}
