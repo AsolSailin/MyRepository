0104b90e40c2347c27239a5441d39dad9d04e091
AsolSailin <101903156+AsolSailin@users.noreply.github.com>
2022-04-12 17:41:26 +0300

    Add files via upload

---
+++ Bars_Task3/IRequestHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading;
+
+namespace Bars_Task3
+{
+    public interface IRequestHandler
+    {
+        string HandleRequest(string message, string[] arguments);
+    }
+}
---
+++ Bars_Task3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Bars_Task3
+{
+    class Program
+    {
+        private static readonly DummyRequestHandler RequestHandler =
+            new DummyRequestHandler();
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст запроса для отправки. " +
+                              "Для выхода введите /exit");
+            string request;
+
+            while ((request = Console.ReadLine()) != "/exit")
+            {
+                CreateRequest(request);
+                Console.WriteLine("Введите текст запроса для отправки. " +
+                                  "Для выхода введите /exit");
+            }
+
+            Console.WriteLine("Конец работы");
+        }
+
+        private static void CreateRequest(string message)
+        {
+            Console.WriteLine("Будет послано сообщение '" + message + "'");
+            Console.WriteLine("Введите аргументы сообщения. " +
+                              "Если аргументы не нужны - введите /end");
+            var args = new List<string>();
+            string argument;
+
+            while ((argument = Console.ReadLine()) != "/end")
+            {
+                args.Add(argument);
+                Console.WriteLine("Введите следующий аргумент сообщения. " +
+                                  "Для окончания добавления аргументов введите /end");
+            }
+
+            var id = Guid.NewGuid().ToString("D");
+            Console.WriteLine("Было отправлено сообщение '" + message + "'. " +
+                              "Присвоен идентификатор " + id);
+            ThreadPool.QueueUserWorkItem(_ => HandleRequest(id, message, args.ToArray()));
+        }
+
+        private static void HandleRequest(string id, string message, string[] args)
+        {
+            try
+            {
+                var answer = RequestHandler.HandleRequest(message, args);
+                Console.WriteLine("Сообщение с идентификатором " + id +
+                                  " получило ответ - " + answer);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Сообщение с идентификатором " + id +
+                                  " упало с ошибкой: " + exception.Message);
+            }
+        }
+    }
+}
